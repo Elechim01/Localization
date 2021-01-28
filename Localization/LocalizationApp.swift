ca35e7a283c3c9d3d69e2dc60e911e0619253985
Elechim01 <mikymanny015@gmail.com>
2021-01-28 22:23:35 +0100

    Initial Commit

---
+++ Localization/LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationApp.swift
+//  Localization
+//
+//  Created by Michele Manniello on 28/01/21.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
